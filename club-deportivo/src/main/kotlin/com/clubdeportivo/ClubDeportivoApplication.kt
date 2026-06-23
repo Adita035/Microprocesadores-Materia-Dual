7aff450f22534dd6db8e8c9b12b378f43fbe3975
Adita035 <152664465+Adita035@users.noreply.github.com>
2026-06-23 17:24:20 -0600

    drafr: Se agrega proyecto

---
+++ club-deportivo/src/main/kotlin/com/clubdeportivo/ClubDeportivoApplication.kt
@@ -0,0 +1,11 @@
+package com.clubdeportivo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClubDeportivoApplication
+
+fun main(args: Array<String>) {
+    runApplication<ClubDeportivoApplication>(*args)
+}
