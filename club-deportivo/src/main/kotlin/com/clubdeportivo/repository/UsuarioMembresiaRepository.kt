58f909edd2c2e6e5735059be52c368579297bb91
Adita035 <152664465+Adita035@users.noreply.github.com>
2026-07-30 22:36:12 -0600

    draft: Se agrega modulo de membresia

---
+++ club-deportivo/src/main/kotlin/com/clubdeportivo/repository/UsuarioMembresiaRepository.kt
@@ -0,0 +1,13 @@
+package com.clubdeportivo.repository
+
+import com.clubdeportivo.entity.UsuarioMembresia
+import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import org.springframework.stereotype.Repository
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+
+@Repository
+interface UsuarioMembresiaRepository : ReactiveCrudRepository<UsuarioMembresia, Long> {
+    fun findByUsuarioId(usuarioId: Long): Flux<UsuarioMembresia>
+    fun findFirstByUsuarioIdAndEstado(usuarioId: Long, estado: String): Mono<UsuarioMembresia>
+}
